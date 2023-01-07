987db8c5fff56c33d0106ca3413eb04f0a39f7f4
Joe Ford <ford153focus@gmail.com>
2023-01-08 01:04:09 +0300

    delay script

--- injections/avito.ru/js/autosearch_sort.js
+++ injections/avito.ru/js/autosearch_sort.js
@@ -1,21 +1,23 @@
-let items = document.querySelectorAll('[data-marker="favorite-search"]').frtToArray();
+setTimeout(() => {
+    let items = document.querySelectorAll('[data-marker="favorite-search"]').frtToArray();
 
-items
-    .sort((item1, item2) => {
-        let cat1 = item1.querySelector('[data-marker="favorite-search/title"] a').innerText;
-        let title1 = item1.querySelector('[data-marker="favorite-search/title"]').innerText;
-
-        let cat2 = item2.querySelector('[data-marker="favorite-search/title"] a').innerText;
-        let title2 = item2.querySelector('[data-marker="favorite-search/title"]').innerText;
-
-        return cat1===cat2 ? title1.localeCompare(title2) : cat1.localeCompare(cat2);
-    })
-    .sort((item1, item2) => {
-        let newAdsIndicator1 = item1.querySelectorAll('div[class^="index-count"] div[class^="index-blue-round"]').length;
-        let newAdsIndicator2 = item2.querySelectorAll('div[class^="index-count"] div[class^="index-blue-round"]').length;
-
-        return newAdsIndicator2-newAdsIndicator1;
-    })
-    .map((item) => {
-        items[0].parentElement.appendChild(item);
-    });
+    items
+        .sort((item1, item2) => {
+            let cat1 = item1.querySelector('[data-marker="favorite-search/title"] a').innerText;
+            let title1 = item1.querySelector('[data-marker="favorite-search/title"]').innerText;
+    
+            let cat2 = item2.querySelector('[data-marker="favorite-search/title"] a').innerText;
+            let title2 = item2.querySelector('[data-marker="favorite-search/title"]').innerText;
+    
+            return cat1===cat2 ? title1.localeCompare(title2) : cat1.localeCompare(cat2);
+        })
+        .sort((item1, item2) => {
+            let newAdsIndicator1 = item1.querySelectorAll('div[class^="index-count"] div[class^="index-blue-round"]').length;
+            let newAdsIndicator2 = item2.querySelectorAll('div[class^="index-count"] div[class^="index-blue-round"]').length;
+    
+            return newAdsIndicator2-newAdsIndicator1;
+        })
+        .map((item) => {
+            items[0].parentElement.appendChild(item);
+        });
+}, 531);

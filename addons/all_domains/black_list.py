4346612d8ef85680c3741bcb9c616e34d553d777
Joe Ford <ford153focus@gmail.com>
2023-02-02 23:34:05 +0300

    rework black list

--- addons/all_domains/black_list.py
+++ addons/all_domains/black_list.py
@@ -3,40 +3,35 @@ from db_utils import db
 
 
 class BlackList:
-    async def blacklist1(self, flow: http.HTTPFlow) -> None:
+    async def is_blocked(self, flow: http.HTTPFlow) -> bool:
         db.cursor.execute(
-            "SELECT * FROM hosts_blacklist WHERE host = %s",
+            "SELECT COUNT(*) FROM hosts_blacklist WHERE host = %s",
             (flow.request.host,)
         )
 
-        _ = db.cursor.fetchall()
+        result = db.cursor.fetchone()
 
-        if db.cursor.rowcount > 0:
-            flow.response = http.Response.make(
-                403,  # (optional) status code
-                b"Forbidden",  # (optional) content
-                # (optional) headers
-                {"Content-Type": "text/plain; charset=utf-8"},
-            )
+        if result[0] > 0: return True
 
-    async def blacklist2(self, flow: http.HTTPFlow) -> None:
         core_host = flow.request.host.split(".")[-2]+"."+flow.request.host.split(".")[-1]
 
         db.cursor.execute(
-            "SELECT * FROM hosts_blacklist_2 WHERE host = %s",
+            "SELECT COUNT(*) FROM hosts_blacklist_2 WHERE host = %s",
             (core_host,)
         )
 
-        _ = db.cursor.fetchall()
+        result = db.cursor.fetchone()
 
-        if db.cursor.rowcount > 0:
-            flow.response = http.Response.make(
-                403,  # (optional) status code
-                b"Forbidden",  # (optional) content
-                # (optional) headers
-                {"Content-Type": "text/plain; charset=utf-8"},
-            )
+        if result[0] > 0: return True
+
+        if "nitropay" in flow.request.host: return True
+
+        return False
 
     async def request(self, flow: http.HTTPFlow) -> None:
-        await self.blacklist1(flow)
-        await self.blacklist2(flow)
+        if (await self.is_blocked(flow) == True):
+            flow.response = http.Response.make(
+                204,  # (optional) status code
+                "",  # (optional) content
+                {"Content-Type": "text/plain; charset=utf-8"}, # (optional) headers
+            )

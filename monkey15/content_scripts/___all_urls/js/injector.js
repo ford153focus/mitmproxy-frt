7c31be0245e82a34302c0744e382ca922349ce01
Joe Ford <ford153focus@gmail.com>
2023-02-06 16:50:50 +0300

    rework injector

--- monkey15/content_scripts/___all_urls/js/injector.js
+++ monkey15/content_scripts/___all_urls/js/injector.js
@@ -12,20 +12,27 @@ class InjectorUtils {
         return pattern.test(window.location.href);
     }
 
-    static injectScript(url, defer = true, fetchPriority = 'low') {
-        let script = document.createElement('script');
+    static injectScript(attributes) {
+        let tag = document.createElement('script');
 
-        script.src = chrome.runtime.getURL(url);
-        script.defer = defer;
-        script.fetchpriority = fetchPriority;
+        for (const [attrName, attrValue] of Object.entries(attributes)) {
+            tag[attrName] = attrValue;
+        }
 
-        document.head.appendChild(script);
+        tag.src = chrome.runtime.getURL(attributes.src);
+        document.head.appendChild(tag);
     }
 
-    static injectStyle(url) {
-        window._frt.utils.loadStyleSheet(
-            chrome.runtime.getURL(url)
-        );
+    static injectStyle(attributes) {
+        let tag = document.createElement('link');
+        tag.rel = 'stylesheet';
+
+        for (const [attrName, attrValue] of Object.entries(attributes)) {
+            tag[attrName] = attrValue;
+        }
+
+        tag.href = chrome.runtime.getURL(attributes.href);
+        document.head.appendChild(tag);
     }
 
     static async injectHypertext(url) {
@@ -53,12 +60,19 @@ class InjectorUtils {
 }
 
 class Injectors {
+    static async twitch() {
+        if (window.location.host !== 'www.twitch.tv') return;
+
+        InjectorUtils.injectScript({src: '/web_accessible_resources/twitch.tv/js/script.js'});
+        InjectorUtils.injectStyle({href: '/web_accessible_resources/twitch.tv/css/directory.css'});
+    }
+
     static async yandex_market() {
         if (window.location.host !== 'market.yandex.ru') return;
 
         if (window.location.pathname.startsWith('/product--karta-pamiati-') ||
             window.location.pathname.startsWith('/product--tverdotelnyi-nakopitel-')) {
-            InjectorUtils.injectScript('/web_accessible_resources/market.yandex.ru/js/price_per_gb.js');
+            InjectorUtils.injectScript({src: '/web_accessible_resources/market.yandex.ru/js/price_per_gb.js'});
         }
     }
 
@@ -68,13 +82,16 @@ class Injectors {
         if (!window.location.href.startsWith('https://5ka.ru/special_offers')) return;
 
         await InjectorUtils.injectHypertext('/web_accessible_resources/habr.com/html/fav_table.html');
-        InjectorUtils.injectStyle('/web_accessible_resources/pixlr.com/css/styles.css');
-        InjectorUtils.injectScript('/web_accessible_resources/pixlr.com/js/adblock.js');    
+        InjectorUtils.injectStyle({href: '/web_accessible_resources/pixlr.com/css/styles.css'});
+        InjectorUtils.injectScript({src: '/web_accessible_resources/pixlr.com/js/adblock.js'});
     }
 
     static async main() {
         // inject shared utils
-        InjectorUtils.injectScript('/web_accessible_resources/_all_urls/js/utils.js', true, 'high');
+        InjectorUtils.injectScript({
+            src: '/web_accessible_resources/_all_urls/js/utils.js', 
+            async: true, 
+        });
 
         // call all other injectors
         let injectors = Object.getOwnPropertyNames(Injectors);

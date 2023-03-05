96ac208c47de124d45a589cdf339d73ae04d2eb7
Joe Ford <ford153focus@gmail.com>
2023-03-06 01:34:33 +0300

    nothing in background is required now

--- monkey15/background/script/background.js
+++ monkey15/background/script/background.js
@@ -1,22 +1,3 @@
 /*eslint no-console: 0*/
 
-class Background {
-    handleMessage(request, sender, sendResponse) {
-        this.notify(request.title, request.message);
-
-        console.log(sender);
-        console.log(sendResponse);
-    }
-
-    notify(title, message) {
-        chrome.notifications.create('', {
-            message: message,
-            title  : title,
-            type   : 'basic'
-        });
-    }
-}
-
-let bg = new Background();
-
-chrome.runtime.onMessage.addListener(bg.handleMessage);
+// nothing is required to run in background at this time

687dfe6a9e7522f687c09af05130fe97a7088b49
Joe Ford <ford153focus@gmail.com>
2023-03-07 22:25:45 +0300

    wrap script to closure

--- monkey15/web_accessible_resources/musicbrainz.org/js/marker.js
+++ monkey15/web_accessible_resources/musicbrainz.org/js/marker.js
@@ -1,23 +1,25 @@
-let node;
-let res;
+(() => {
+    let node;
+    let res;
 
-res = document.evaluate("//td[text()='Single']", document);
-while (node = res.iterateNext()) {
-      for (let el of node.parentElement.querySelectorAll('td')) {
-          el.style['background-color'] = '#aca';
-      }
-}
-
-res = document.evaluate("//td[text()='EP']", document);
-while (node = res.iterateNext()) {
-      for (let el of node.parentElement.querySelectorAll('td')) {
-          el.style['background-color'] = '#ada';
-      }
-}
-
-res = document.evaluate("//td[text()='Album']", document);
-while (node = res.iterateNext()) {
-    for (let el of node.parentElement.querySelectorAll('td')) {
-        el.style['background-color'] = '#afa';
+    res = document.evaluate("//td[text()='Single']", document);
+    while (node = res.iterateNext()) {
+          for (let el of node.parentElement.querySelectorAll('td')) {
+              el.style['background-color'] = '#aca';
+          }
     }
-}
+
+    res = document.evaluate("//td[text()='EP']", document);
+    while (node = res.iterateNext()) {
+          for (let el of node.parentElement.querySelectorAll('td')) {
+              el.style['background-color'] = '#ada';
+          }
+    }
+
+    res = document.evaluate("//td[text()='Album']", document);
+    while (node = res.iterateNext()) {
+        for (let el of node.parentElement.querySelectorAll('td')) {
+            el.style['background-color'] = '#afa';
+        }
+    }
+})();

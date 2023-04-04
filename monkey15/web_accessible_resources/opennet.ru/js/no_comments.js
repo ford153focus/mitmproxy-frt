76679ae3ebc4b1b0610859301dd6250334d79700
Joe Ford <ford153focus@gmail.com>
2023-04-04 03:15:24 +0300

    conditional access

--- monkey15/web_accessible_resources/opennet.ru/js/no_comments.js
+++ monkey15/web_accessible_resources/opennet.ru/js/no_comments.js
@@ -2,32 +2,32 @@ setTimeout(() => {
     document
         .evaluate("//*[text()='Обсуждение']", document)
         .iterateNext()
-        .parentElement.parentElement.parentElement.parentElement
+        .parentElement?.parentElement?.parentElement?.parentElement
         .nextElementSibling
         .remove();
 
     document
         .evaluate("//*[text()='Обсуждение']", document)
         .iterateNext()
-        .parentElement.parentElement.parentElement.parentElement
+        .parentElement?.parentElement?.parentElement?.parentElement
         .remove();
 
     document
         .evaluate("//*[text()=' Добавить комментарий']", document)
         .iterateNext()
-        .parentElement.parentElement.parentElement.parentElement
+        .parentElement?.parentElement?.parentElement?.parentElement
         .nextElementSibling
         .remove();
 
     document
         .evaluate("//*[text()=' Добавить комментарий']", document)
         .iterateNext()
-        .parentElement.parentElement.parentElement.parentElement
+        .parentElement?.parentElement?.parentElement?.parentElement
         .remove();
 
     document
         .getElementById("lenta_nav2")
-        .previousElementSibling.previousElementSibling.previousElementSibling
+        .previousElementSibling?.previousElementSibling?.previousElementSibling
         .remove();
 
     document

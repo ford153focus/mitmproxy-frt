6f0caee3fd8f5fa7149961035d81034465188621
Joe Ford <ford153focus@gmail.com>
2023-05-01 02:36:56 +0300

    wrap to setTimeout

--- monkey15/web_accessible_resources/fandom.com/genshin-impact/js/event_list.js
+++ monkey15/web_accessible_resources/fandom.com/genshin-impact/js/event_list.js
@@ -1,22 +1,25 @@
-let table = document.getElementById('Current').parentElement.nextElementSibling;
+setTimeout(() => {
+    let table = document.getElementById('Current').parentElement.nextElementSibling;
+    let rows = table.querySelectorAll('tr');
 
-for (let row of table.querySelectorAll('tr'))
-{
-    for (let cell of row.querySelectorAll('td'))
+    for (let row of rows)
     {
-        if (cell.innerText === 'Web, Submission')
+        for (let cell of row.querySelectorAll('td'))
         {
-            row.style.backgroundColor = 'darkred';
-        }
+            if (cell.innerText === 'Web, Submission')
+            {
+                row.style.backgroundColor = 'darkred';
+            }
 
-        if (cell.innerText === 'In-Game, Battle Pass')
-        {
-            row.style.backgroundColor = 'darkred';
-        }
-        
-        if (cell.innerText === 'In-Game, Test Run')
-        {
-            row.style.backgroundColor = 'darkorange';
+            if (cell.innerText === 'In-Game, Battle Pass')
+            {
+                row.style.backgroundColor = 'darkred';
+            }
+
+            if (cell.innerText === 'In-Game, Test Run')
+            {
+                row.style.backgroundColor = 'darkorange';
+            }
         }
     }
-}
+}, 153);

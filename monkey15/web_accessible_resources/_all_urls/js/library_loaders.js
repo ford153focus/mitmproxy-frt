9b30e5d0503c2c2fe92eb2ebdf047837d63cfde9
Joe Ford <ford153focus@gmail.com>
2023-05-08 13:12:08 +0300

    fix loaders

--- monkey15/web_accessible_resources/_all_urls/js/library_loaders.js
+++ monkey15/web_accessible_resources/_all_urls/js/library_loaders.js
@@ -2,22 +2,19 @@ if (!window._frt) window._frt = {};
 
 window._frt.LibraryLoaders = class {
     static bootStrap() {
-        window._frt.Injectors.injectInternalStyleSheet({
+        window._frt.Injectors.injectSheet({
             href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css',
-            is_cdn: true,
         });
 
-        window._frt.Injectors.injectInternalScript({
+        window._frt.Injectors.injectScript({
             src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js',
             async: true,
-            is_cdn: true,
         });
     }
 
     static fontAwesome() {
-        window._frt.Injectors.injectInternalStyleSheet({
+        window._frt.Injectors.injectSheet({
             href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css',
-            is_cdn: true,
         });
     }
 

49e2d0c5f37a56b3bf9a46effc0b1eeae7776e15
Joe Ford <ford153focus@gmail.com>
2024-05-23 11:35:46 +0300

    remove entry of unexisting folder

--- monkey15/manifest.js
+++ monkey15/manifest.js
@@ -28,7 +28,6 @@ let manifest = {
             'resources': [
                 'node_modules/*',
                 'web_accessible_resources/*',
-                'lib/*'
             ],
             'matches': [
                 '<all_urls>'
